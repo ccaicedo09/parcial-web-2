58cb77bef47331cee9a95e7494876300cf5c7e7d
unknown <u20231213568@usco.edu.co>
2024-10-29 09:08:13 -0500

    initial commit

---
+++ src/main/java/org/usco/parcialdos/ParcialDosApplication.java
@@ -0,0 +1,13 @@
+package org.usco.parcialdos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialDosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParcialDosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/usco/parcialdos/ParcialDosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.usco.parcialdos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialDosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
